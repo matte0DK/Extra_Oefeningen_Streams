9fc3bc7c55b74ea3c6f74febbd1a572797228e80
matte0DK <72343291+matte0DK@users.noreply.github.com>
2020-12-11 16:27:43 +0100

    correction extra oefeningen streams

--- src/be/intecbrussel/Oefeningen.java
+++ src/be/intecbrussel/Oefeningen.java
@@ -1,57 +1,87 @@
 package be.intecbrussel;
 
 import java.util.Optional;
+import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Oefeningen {
     public static void main(String[] args) {
         int[] numbers = new int[]{23, 22, 51, 73, 54, 48, 61, 84, 95, 11};
-        String[] names = new String[]{"Manu", "Audric", "Dieter", "Tonykjagjksjbikjsboijzbeobsdovcns", "Mert"};
+        String[] names = new String[]{"Manu", "Audric", "Dieter", "881", "Mert"};
 
-        // Opdracht 1
+        // OPDRACHT 1 //
         IntStream.of(numbers)
-                .map(i -> i *= 5)
+                .map(i -> i * 5)
                 .filter(i -> i > 50)
                 .forEach(i -> System.out.print(i + " "));
         System.out.println(" ");
 
-        // Opdracht 2
-        int[] intArr = IntStream.of(numbers)
-                .map(i -> i /= 2)
+        // OPDRACHT 2 //
+        double[] intArr = IntStream.of(numbers)
+                .mapToDouble(i -> (double) i / 2) // => typecasten, geeft aan dat dit een double moet zijn!
                 .toArray();
+        DoubleStream.of(intArr)
+                .forEach(d -> System.out.print(d + " "));
+        System.out.println(" ");
 
-        // Opdracht 3
+        // OPDRACHT 3 //
         Optional<String> singleString = Stream.of(names)
                 .limit(2)
                 .map(String::toUpperCase)
                 .reduce((acc, el) -> acc + el);
 
         if (singleString.isPresent()) {
-            System.out.println("singleString = " + singleString);
-        }
-        // Opdracht 4
-        IntStream.of(numbers)
+            System.out.println("singleString = " + singleString.get());
+        } // => my solution
+
+        String newString = Stream.of(names)
+                .limit(2)
+                .map(String::toUpperCase)
+                .reduce("", (acc, el) -> acc + el);
+
+        // OPDRACHT 4 //
+        int sum = IntStream.of(numbers)
                 .skip(2)
                 .sum();
+        System.out.println(sum);
 
-        // Opdracht 5
-        Stream<String> stringToIntStream = Stream.of(names);
 
-        stringToIntStream.mapToInt(String::hashCode)
-                .peek(i -> {
-                    try {
-                        checkSize(i);
-                    } catch (TooBigTooHandleException e) {
-                        e.getMessage();
-                    }
-                })
-                .forEach(i -> System.out.print(i + " "));
+        // OPDRACHT 5 //
+        Stream.of(names).filter(i -> convertableToInt(i))
+                .mapToInt(i -> Integer.valueOf(i))
+                .forEach(i -> System.out.println(i + " "));
+
+//        Stream<String> stringToIntStream = Stream.of(names);
+//        stringToIntStream.mapToLong(String::hashCode)
+//                .peek(i -> {
+//                    try {
+//                        checkInt(i);
+//                    } catch (NumberFormatException e) {
+//                        e.getMessage();
+//                    }
+//                })
+//                .forEach(i -> System.out.print(i + " "));
+
+//      <=> niet de vraag! dit is om te checken of een string als een long in een int past...
+
     }
 
-    private static void checkSize(int i) throws TooBigTooHandleException {
-        if (!(i < Integer.MAX_VALUE)) {
-            throw new TooBigTooHandleException("this value is bigger than the max value of an integer.");
+    private static boolean convertableToInt(String s) {
+        try {
+            Integer.valueOf(s);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
         }
     }
+
+//    private static void checkInt(long l) throws NumberFormatException {
+//        if (!(l < Integer.MAX_VALUE)) {
+//            throw new NumberFormatException("this value is not valid for an integer.");
+//        }
+//    }
+
+//    <=> niet de vraag! dit is om te checken of een string als een long in een int past...
+
 }
